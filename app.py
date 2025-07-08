b9a9c150d03ec324888cf5723d201870bd868d33
Treshnanda <treshnanda@gmail.com>
2025-07-08 18:00:20 +0800

    buig fixes

--- app.py
+++ app.py
@@ -2,6 +2,117 @@ import streamlit as st
 import pandas as pd
 import joblib
 import requests
+import numpy as np
+
+class DecisionTreeRegressorScratch:
+    def __init__(self, max_depth=None, min_samples_split=2, max_features=None):
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.max_features = max_features
+        self.tree_ = None
+
+    class Node:
+        def __init__(self, feature_idx=None, threshold=None, left=None, right=None, value=None):
+            self.feature_idx = feature_idx
+            self.threshold = threshold
+            self.left = left
+            self.right = right
+            self.value = value
+
+    def fit(self, X, y):
+        n_features = X.shape[1]
+        if self.max_features is None:
+            self.max_features = n_features
+        else:
+            self.max_features = min(self.max_features, n_features)
+        self.tree_ = self._build_tree(X, y, depth=0)
+        return self
+
+    def _build_tree(self, X, y, depth):
+        n_samples, n_features = X.shape
+        if (self.max_depth is not None and depth >= self.max_depth) or n_samples < self.min_samples_split:
+            return self.Node(value=y.mean())
+
+        feat_idxs = np.random.choice(n_features, self.max_features, replace=False)
+
+        best_feat, best_thresh, best_mse = None, None, np.inf
+        for feat in feat_idxs:
+            thresholds = np.unique(X[:, feat])
+            for thr in thresholds:
+                left_mask = X[:, feat] <= thr
+                if left_mask.sum() == 0 or left_mask.sum() == n_samples:
+                    continue
+                y_left, y_right = y[left_mask], y[~left_mask]
+                mse_left = np.var(y_left) * y_left.shape[0]
+                mse_right = np.var(y_right) * y_right.shape[0]
+                mse_total = (mse_left + mse_right) / n_samples
+                if mse_total < best_mse:
+                    best_feat, best_thresh, best_mse = feat, thr, mse_total
+
+        if best_feat is None:
+            return self.Node(value=y.mean())
+
+        left_mask = X[:, best_feat] <= best_thresh
+        left = self._build_tree(X[left_mask], y[left_mask], depth+1)
+        right = self._build_tree(X[~left_mask], y[~left_mask], depth+1)
+        return self.Node(feature_idx=best_feat, threshold=best_thresh, left=left, right=right)
+
+    def _predict_one(self, x, node):
+        if node.value is not None:
+            return node.value
+        if x[node.feature_idx] <= node.threshold:
+            return self._predict_one(x, node.left)
+        else:
+            return self._predict_one(x, node.right)
+
+    def predict(self, X):
+        return np.array([self._predict_one(x, self.tree_) for x in X])
+
+class RandomForestRegressorScratch:
+    def __init__(self, n_estimators=10, max_depth=None, min_samples_split=2,
+                 max_features='sqrt', bootstrap=True, random_state=None):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.bootstrap = bootstrap
+        self.trees_ = []
+        self.random_state = random_state
+        self.max_features = max_features
+
+    def fit(self, X, y):
+        np.random.seed(self.random_state)
+        n_samples, n_features = X.shape
+
+        if self.max_features == 'sqrt':
+            max_feats = int(np.sqrt(n_features))
+        elif self.max_features == 'log2':
+            max_feats = int(np.log2(n_features))
+        elif isinstance(self.max_features, int):
+            max_feats = self.max_features
+        else:
+            max_feats = n_features
+
+        self.trees_ = []
+        for i in range(self.n_estimators):
+            if self.bootstrap:
+                idxs = np.random.choice(n_samples, size=n_samples, replace=True)
+                X_sample, y_sample = X[idxs], y[idxs]
+            else:
+                X_sample, y_sample = X, y
+
+            tree = DecisionTreeRegressorScratch(
+                max_depth=self.max_depth,
+                min_samples_split=self.min_samples_split,
+                max_features=max_feats
+            )
+            tree.fit(X_sample, y_sample)
+            self.trees_.append(tree)
+        return self
+
+    def predict(self, X):
+        preds = np.array([tree.predict(X) for tree in self.trees_])
+        return np.mean(preds, axis=0)
+
 
 SENSOR_KEYS = ['aqi', 'co', 'dew', 'h', 'no2', 'o3', 'pm10', 'pm25', 'so2', 't', 'w']
 
